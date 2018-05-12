cffe25906ceb4095a6750a4b69fae3b1fcd4a9ca
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2018-05-12 22:37:50 +0700

    Add files via upload

---
+++ Java_Course_TH03/src/thuan/nguyenngoc/Point.java
@@ -0,0 +1,28 @@
+package thuan.nguyenngoc;
+
+public class Point {
+	private float x;
+	private float y;
+
+	public Point() {
+		this.x = Math.round(Math.random());
+		this.y = Math.round(Math.random());
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+}
