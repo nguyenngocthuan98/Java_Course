c819fb65a7b044065acc4556b804c9b62e1b4d96
nguyenngocthuan98 <37949712+nguyenngocthuan98@users.noreply.github.com>
2018-05-19 03:17:17 +0700

    Add files via upload
    bài tập thay cho TH05

---
+++ BTap/src/thuan/nguyenngoc/ISearch.java
@@ -0,0 +1,9 @@
+package thuan.nguyenngoc;
+
+public interface ISearch {
+	
+	public Object timKiem(int from, int to);
+	public Object timKiem(boolean gioitinh);
+	public Object timKiem(String hoten);
+	
+}
